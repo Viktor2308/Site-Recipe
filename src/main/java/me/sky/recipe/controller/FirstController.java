1a6102a3d5cf554a8fbc8a42beb0915cda0a435e
Viktor <viktor_23_08@mail.ru>
2023-02-01 16:16:54 +0100

    homework 1

---
+++ src/main/java/me/sky/recipe/controller/FirstController.java
@@ -0,0 +1,21 @@
+package me.sky.recipe.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String info(){
+        return "Victor Derkachev, " +
+                "Site recipe, " +
+                "01.02.2023, " +
+                "Site recipe, Java, Spring Boot.";
+    }
+}
